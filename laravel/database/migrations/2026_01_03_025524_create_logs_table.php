bdb5769ac63a1dfaa9eb77c29ec7506019d099d0
imayyuu <putriayuwandarii@gmail.com>
2026-01-10 12:25:21 +0800

    logging

--- laravel/database/migrations/2026_01_03_025524_create_logs_table.php
+++ laravel/database/migrations/2026_01_03_025524_create_logs_table.php
@@ -18,7 +18,6 @@ return new class extends Migration {
             $table->text("log_ip");
             $table->text("log_request");
             $table->timestamps();
-            $table->softDeletes();
         });
     }
 
